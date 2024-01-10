8e88b910c31b27a05f89596a6088c25ce83756b9
Badmus_Farouk1 <72664875+Art101-ui@users.noreply.github.com>
2024-01-10 15:05:06 +0100

    feat: route org for coin/converter nav links

---
+++ app/(routes)/(home)/layout.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx"
+
+
+type ButtonProps ={
+    children: React.ReactNode,
+    page: string
+}
+
+export default function Layout({children}:{children:React.ReactNode}){
+
+    
+    return(
+        <div className=" px-[72px] ">
+            <div className=" bg-white flex w-[500px] h-[53px] rounded-md">
+                <Button page='coin'>Coins</Button>
+                <Button page='converter'>Converter</Button>
+            </div>
+            <div>
+              {children}
+            </div>
+        </div>
+    )
+}
+
+
+function Button({children, page}:ButtonProps){
+   return(
+     <button className={clsx("flex justify-center items-center hover:bg-blue-700 w-1/2 h-full font-bold py-3 px-4 rounded-md",
+       {'bg-[#6161D6] text-white' : page ==='coin'},
+       {' text-[#6161D6]' : page ==='converter'}
+     )}>
+        {children}
+     </button>
+   )
+}
