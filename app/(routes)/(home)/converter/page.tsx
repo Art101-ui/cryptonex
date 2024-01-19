85a58bed6f2511c7ca39c3e2c0ace19dcadaca24
Badmus_Farouk1 <72664875+Art101-ui@users.noreply.github.com>
2024-01-19 07:20:22 +0100

    feat: add navigation links functionality

--- app/(routes)/(home)/converter/page.tsx
+++ app/(routes)/(home)/converter/page.tsx
@@ -1,5 +1,10 @@
 export default function Converter(){
     return(
-        <div>Converter</div>
+        <>
+          <div>
+            <h1 className=" text-[#424286] text-[20px]">Online currency converter</h1>
+            <h3 className=" text-[#424286]/80 text-[16px]">09/29/2023 14:15</h3>  
+          </div> 
+        </>
     )
 }
--- app/(routes)/(home)/layout.tsx
+++ app/(routes)/(home)/layout.tsx
@@ -1,19 +1,38 @@
+'use client'
+
 import clsx from "clsx"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
-type ButtonProps ={
-    children: React.ReactNode,
-    page: string
-}
 
 export default function Layout({children}:{children:React.ReactNode}){
-
-    
+   
+  const links = [
+    { name: 'Coin', href: '/' },
+    {
+      name: 'Converter',
+      href: '/converter',
+      
+    },
+  ];
+    const pathname =  usePathname()
     return(
         <div className=" px-[72px] ">
             <div className=" bg-white flex w-[450px] h-[45px] rounded-md mb-6">
-                <Button page='coin'>Coins</Button>
-                <Button page='converter'>Converter</Button>
+                {links.map(link=>{
+                  return(
+                    <Link 
+                     key={link.name}
+                     href={link.href}
+                     className={clsx("flex justify-center items-center w-1/2 h-full font-bold py-3 px-4 rounded-md",
+                        {'bg-[#6161D6] text-white' : pathname === link.href},
+                      )}>
+                        {link.name}
+                      
+                    </Link>
+                  )
+                })}
             </div>
             <div>
               {children}
@@ -23,13 +42,13 @@ export default function Layout({children}:{children:React.ReactNode}){
 }
 
 
-function Button({children, page}:ButtonProps){
-   return(
-     <button className={clsx("flex justify-center items-center hover:bg-blue-700 w-1/2 h-full font-bold py-3 px-4 rounded-md",
-       {'bg-[#6161D6] text-white' : page ==='coin'},
-       {' text-[#6161D6]' : page ==='converter'}
-     )}>
-        {children}
-     </button>
-   )
-}
+// function Button({children, page}:LinkProps){
+//    return(
+//      <div className={clsx("flex justify-center items-center hover:bg-blue-700 w-1/2 h-full font-bold py-3 px-4 rounded-md",
+//        {'bg-[#6161D6] text-white' : page ==='coin'},
+//        {' text-[#6161D6]' : page ==='converter'}
+//      )}>
+//         {children}
+//      </div>
+//    )
+// }
